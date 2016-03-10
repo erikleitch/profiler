3768444102ec768c7d135ec40d59bc3b747d83bb
erikleitch <eleitch@basho.com>
2016-03-10 11:25:12 -0800

    Added macro for FNNAME

--- include/profiler.hrl
+++ include/profiler.hrl
@@ -12,5 +12,10 @@ perf_profile(_Tuple) ->
 perf_fnstring({_Label, {Mod, Fn, _Arity}}) ->
     atom_to_list(Mod) ++ ":" ++ atom_to_list(Fn).
 
+-define(FNNAME(), profiler:perf_fnstring(process_info(self(), current_function))).
+
+
+
+
     
     
